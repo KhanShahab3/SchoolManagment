f6ba1377c7a2a7690ad552fb7f6fdbc21d235ba1
KhanShahab3 <khanswati7372@gmail.com>
2025-08-23 05:58:23 -0700

    Db Connection

---
+++ SchoolManagment/AppDbContext/AppDb.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SchoolManagment.AppDbContext
+{
+    public class AppDb:DbContext
+    {
+        public AppDb(DbContextOptions<AppDb> options) : base(options)
+        {
+        }
+    }
+}
